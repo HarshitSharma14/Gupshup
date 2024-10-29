f54915cc7b7909cba73419084db07c14880891ae
Harshit <harshitsharmaclg@gmail.com>
2024-10-30 03:56:36 +0530

    just did some basic stuff, adding lots of tailwind (for 15 mins during the lab)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import Auth from './pages/auth/Index'
+import Chat from './pages/chat/Index'
+import Profile from './pages/profile/Index'
 const App = () => {
   return (
-    <div>
-      <Button>click me</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/Auth" />} />      </Routes>
+    </BrowserRouter>
   )
 }
 
